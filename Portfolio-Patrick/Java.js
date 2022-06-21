c4f50fa10de9a117e9184bb41d1e237e291384df
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2022-06-20 23:54:37 -0400

    update

---
+++ Portfolio-Patrick/Java.js
@@ -0,0 +1,6 @@
+function semColoracao(imagem){
+    imagem.src="image/maria-sem-color-gradien.png";
+   }
+   function comColoracao(imagem){
+    imagem.src="image/maria-com-color-gradien.png";
+   }
